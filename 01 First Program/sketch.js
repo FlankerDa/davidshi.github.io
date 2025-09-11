a74c5abcd637656b20b36048f9b64dd8ecbdedfa
davidshi <162340363+FlankerDa@users.noreply.github.com>
2025-09-11 11:54:50 -0600

    1

--- 01 First Program/sketch.js
+++ 01 First Program/sketch.js
@@ -1,17 +1,22 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Coordinates Systems and User Events
+// David Shi
+// September 10, 2025
+//A first look at interactive programs -
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  print(windowWidth, windowHeight, width, height);
 }
 
 function draw() {
-  background(220);
-  fill(100);
-  circle(mouseX, mouseY, 50) 
+  background(200);
+  drawTwoCircles();
+}
+
+function drawTwoCircles(){
+  
+  fill(0, 225, 0);
+  circle(mouseX, mouseY, 30);
+  circle(width/2, height/2, 100);
 }
