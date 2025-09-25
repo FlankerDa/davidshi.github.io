c08926e0ce0212f4437d0e2ff84352aa75d1e424
davidshi <162340363+FlankerDa@users.noreply.github.com>
2025-09-25 12:18:47 -0600

    1

---
+++ 07 working with lmage/sketch.js
@@ -0,0 +1,18 @@
+// Working with Images
+// David Shi
+// 9/24/2025
+
+let lionL, lipnR;
+
+function setup() {
+
+}
+
+async function loadAssets(){
+    lionL = await loadAssets("assets/lion-left.png")
+    lionR = await loadAssets("assets/lion-right.png")
+}
+
+function draw() {
+
+}
