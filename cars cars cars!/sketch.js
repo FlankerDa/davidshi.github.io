f9fe6a32178bfe00e954ebaa078fa03447fffe8b
davidshi <162340363+FlankerDa@users.noreply.github.com>
2025-11-04 13:06:57 -0600

    1

--- cars cars cars!/sketch.js
+++ cars cars cars!/sketch.js
@@ -7,6 +7,7 @@
 
 let eastbound = [];
 let westbound = [];
+let trafficLight;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -18,6 +19,7 @@ function setup() {
     let yWest = random(windowHeight / 4 + 270, windowHeight / 4 + 480);
     westbound.push(new Vehicle(random(windowWidth), yWest, int(random(0, 2)), random(-1, -5)));
   }
+  trafficLight = new TrafficLight(windowWidth/ 2, windowHeight / 4 - 60);
 }
 
 function draw() {
@@ -25,18 +27,29 @@ function draw() {
   fill(56);
   drawRoad();
 
+  trafficLight.display();
+  trafficLight.update();
+
   for (let v of eastbound) {
-    v.action();
+    if (trafficLight.isGreen()){
+      v.action();
+    }else{
+      v.display()
+    };
   }
 
   for (let v of westbound) {
-    v.action();
+    if (trafficLight.isGreen()){
+      v.action();
+    }else{
+      v.display();
+    }
   }
 }
 
-function keyPressed(){
-  if(keyCode === "SPACE"){
-
+function keyPressed() {
+  if (key === ' ') {
+    trafficLight.turnRed();
   }
 }
 
@@ -85,5 +98,61 @@ class Vehicle {
   action() {
     this.move();
     this.display(); 
+  }
+}
+
+function mousePressed() {
+  if (keyIsDown(SHIFT)) {
+    let yWest = random(windowHeight / 4 + 270, windowHeight / 4 + 480);
+    westbound.push(new Vehicle(mouseX, yWest, int(random(0, 2)), random(-1, -5)));
+  } else {
+    let yEast = random(windowHeight / 4, windowHeight / 4 + 200);
+    eastbound.push(new Vehicle(mouseX, yEast, int(random(0, 2)), random(1, 5)));
+  }
+}
+
+class TrafficLight {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.state = "green";
+    this.timer = 0;
+  }
+
+  display() {
+
+    fill(0);
+    rect(this.x - 20, this.y - 60, 40, 100, 10);
+
+    if (this.state === "red") {
+      fill("red");
+    } else {
+      fill("gray");
+    }
+    circle(this.x, this.y - 25, 30);
+
+    if (this.state === "green") {
+      fill("green");
+    } else {
+      fill("gray");
+    }
+    circle(this.x, this.y + 25, 30);
+  }
+  update() {
+    if (this.state === "red") {
+      this.timer--;
+      if (this.timer <= 0) {
+        this.state = "green";
+      }
+    }
+  }
+
+  turnRed() {
+    this.state = "red";
+    this.timer = 120;
+  }
+
+  isGreen() {
+    return this.state === "green";
   }
 }
