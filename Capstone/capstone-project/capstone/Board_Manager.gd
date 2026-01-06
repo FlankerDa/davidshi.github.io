6ee54e454e22101a0a7c49ce76280a391847fc9b
davidshi <162340363+FlankerDa@users.noreply.github.com>
2026-01-06 14:09:38 -0600

    spawning pieces

--- Capstone/capstone-project/capstone/Board_Manager.gd
+++ Capstone/capstone-project/capstone/Board_Manager.gd
@@ -24,6 +24,7 @@ var selected_tile: Vector2i = Vector2i(-1, -1)
 
 func _ready():
 	_assign_grid_positions()
+	_spawn_starting_position()
 
 	for n in get_tree().get_nodes_in_group("tiles"):
 		if n.has_signal("tile_clicked"):
@@ -74,11 +75,63 @@ func _get_model(color: String, kind: String) -> PackedScene:
 			"queen": return white_queen
 			"king": return white_king
 	return null
+	
+var pieces := {}
+
+func _spawn_piece(color: String , kind: String, pos: Vector2i) -> void:
+	var root: Node3D = get_node(piece_root)
+	
+	var p: Piece = piece_scene.instantiate()
+	root.add_child(p)
+	
+	var model_scene := _get_model(color, kind)
+	p.setup(color, kind, pos, model_scene)
+	
+	p.global_position = _get_tile_world_pos(pos) + Vector3(0,10,0)
+	pieces[pos] = p
+	
 			
-			
-			
-			
-			
+func _get_tile_world_pos(pos: Vector2i) -> Vector3:
+	for t in get_tree().get_nodes_in_group("tiles"):
+		if t.grid_pos == pos:
+			return t.global_position
+	
+	return Vector3.ZERO
+
+func _spawn_starting_position() -> void:
+	# Clear old pieces if any
+	for k in pieces.keys():
+		var old = pieces[k]
+		if is_instance_valid(old):
+			old.queue_free()
+	pieces.clear()
+	# Pawns
+	for x in range(8):
+		_spawn_piece("white", "pawn", Vector2i(x, 1))
+		_spawn_piece("black", "pawn", Vector2i(x, 6))
+	# Rooks
+	_spawn_piece("white", "rook", Vector2i(0, 0))
+	_spawn_piece("white", "rook", Vector2i(7, 0))
+	_spawn_piece("black", "rook", Vector2i(0, 7))
+	_spawn_piece("black", "rook", Vector2i(7, 7))
+	# Knights
+	_spawn_piece("white", "knight", Vector2i(1, 0))
+	_spawn_piece("white", "knight", Vector2i(6, 0))
+	_spawn_piece("black", "knight", Vector2i(1, 7))
+	_spawn_piece("black", "knight", Vector2i(6, 7))
+	# Bishops
+	_spawn_piece("white", "bishop", Vector2i(2, 0))
+	_spawn_piece("white", "bishop", Vector2i(5, 0))
+	_spawn_piece("black", "bishop", Vector2i(2, 7))
+	_spawn_piece("black", "bishop", Vector2i(5, 7))
+	# Queens
+	_spawn_piece("white", "queen", Vector2i(3, 0))
+	_spawn_piece("black", "queen", Vector2i(3, 7))
+	# Kings
+	_spawn_piece("white", "king", Vector2i(4, 0))
+	_spawn_piece("black", "king", Vector2i(4, 7))
+
+
 			
 			
 			
